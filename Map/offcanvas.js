2cfebb177c9c21b63f405299575fd8f6d658028a
jinfeiliang <jinl33@nycstudents.net>
2026-04-09 19:04:59 -0400

    Autohide and Close Extra_data info Offcanvas

--- Map/offcanvas.js
+++ Map/offcanvas.js
@@ -5,8 +5,29 @@ let OffCanvas_Extra_Info_DOM = document.querySelector("#OffCanvas_Extra_Info");
 
 
 let Main_Info_Categories = ["place_name", "place.address"]
+
+
+function Close_All_Off_Canvas_Accordion_Extra_Information() {
+  const secondAccordion = document.querySelector("#collapseTwo");
+  const bsCollapse = new bootstrap.Collapse(secondAccordion, {
+    toggle: false,
+  });
+  bsCollapse.hide();
+
+}
+function Is_Only_Extra_Data(object) {
+  let result = true;
+  for (const [key, value] of Object.entries(object)) {
+    if (key != "extra_data") {
+      result = false;
+    }
+  }
+  return result;
+}
+
 async function Load_Data_Off_Canvas(Data) {
   console.log(Data.extra_data.Processing_Method);
+  Close_All_Off_Canvas_Accordion_Extra_Information();
   const API_DATA_MANAGER = new DataProcessor(
     Data,
     Data.extra_data.Processing_Method,
@@ -17,45 +38,51 @@ async function Load_Data_Off_Canvas(Data) {
   OffCanvas_Main_Info_DOM.innerHTML = "";
   OffCanvas_Extra_Info_DOM.innerHTML = "";
 
-  
-
-  const bsOffcanvas = new bootstrap.Offcanvas(Offcanvas_Map_Info_DOM);
-  bsOffcanvas.show();
-}
-
-function Load_Data_Off_Canvas_DEPRECATED(Data) {
-  let Info = Data;
-  console.log("Load Canvas Info");
-  console.log(Info);
-  
-  Offcanvas_Map_Info_Body_DOM.innerHTML = "";
-  for (const [key, value] of Object.entries(Info)) {
-    //console.log(`${key}: ${value}`);
-
+  for (const [key, value] of Object.entries(Standarized_Data)) {
     if (value == null) {
       continue;
     }
-    let Formatted_Key_Name = key;
-    if (Properties_Name[key]) {
-      Formatted_Key_Name = Properties_Name[key];
+    if (key == "metadata") {
+      continue;
     }
-      
+    let Formatted_Key_Name = key;
+
     let HTML = `<div>
       <h2>${Formatted_Key_Name}</h2>
       <p>${JSON.stringify(value)}</p>
     </div>`;
-      
-    // if (key == "address") {
-    //   HTML = `<div>
-    //   <h2>${key}</h2>
-    //   <a target="_blank" href="${Google_Maps_Search_Link(value)}"><p>${value}</p></a>
-    // </div>`;
-    // }
+    OffCanvas_Main_Info_DOM.insertAdjacentHTML("beforeend", HTML);
+  }
+  console.log(Standarized_Data.metadata);
+  if (Standarized_Data.metadata && !Is_Only_Extra_Data(Standarized_Data.metadata)) {
+    document.querySelector("#headingTwo").classList.remove("visually-hidden");
+    console.log("WOW");
+    for (const [key, value] of Object.entries(Standarized_Data.metadata)) {
+      if (value == null) {
+        continue;
+      }
+      if (key == "extra_data") {
+        continue;
+      }
+      let Formatted_Key_Name = key;
 
-    Offcanvas_Map_Info_Body_DOM.insertAdjacentHTML("beforeend", HTML);
+      let HTML = `<div>
+      <h2>${Formatted_Key_Name}</h2>
+      <p>${JSON.stringify(value)}</p>
+    </div>`;
+      OffCanvas_Extra_Info_DOM.insertAdjacentHTML("beforeend", HTML);
+    }
+
+
+  } else {
+    document.querySelector("#headingTwo").classList.add("visually-hidden");
+    OffCanvas_Extra_Info_DOM.innerHTML = "This center does not have extra information";
+    
   }
 
   const bsOffcanvas = new bootstrap.Offcanvas(Offcanvas_Map_Info_DOM);
   bsOffcanvas.show();
 }
+
+
 

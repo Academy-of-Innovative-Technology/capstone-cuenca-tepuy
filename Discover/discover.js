5f666e4d2afba350848aa259609eac8c287a20cd
Jin Liang <144455501+jinfeiliang@users.noreply.github.com>
2026-05-07 13:50:37 -0400

    Discover Cards Hyperlink and some pictures

--- Discover/discover.js
+++ Discover/discover.js
@@ -2,10 +2,35 @@ let Discovery_Container_DOM = document.querySelector("#Discovery_Container");
 
 let Discovery_Content_List = [
   {
-    header: "Header",
-    title: "Title",
+    header: "Map",
+    title: "Food Bank For NYC",
     subtitle: "Subtitle",
     img_link: "gg",
+    hyperlink: "https://www.foodbanknyc.org/news-and-stories/",
+    description: "Esternocleidomastodeo",
+  },
+  {
+    header: "Data",
+    title: "New York City Council",
+    subtitle: "Subtitle",
+    img_link: "gg",
+    hyperlink: "https://council.nyc.gov/data/emergency-food-in-nyc/",
+    description: "Esternocleidomastodeo",
+  },
+  {
+    header: "Information",
+    title: "Metropolitan Transportation Authority",
+    subtitle: "Bathroom locations",
+    img_link: "gg",
+    hyperlink: "https://www.mta.info/agency/new-york-city-transit/bathrooms",
+    description: "Esternocleidomastodeo",
+  },
+  {
+    header: "Map",
+    title: "Metropolitan Transportation Authority",
+    subtitle: "Subway MTA Maps",
+    img_link: "../Images/Subway_Diagram.jpg",
+    hyperlink: "https://www.mta.info/maps",
     description: "Esternocleidomastodeo",
   },
 ];
@@ -19,33 +44,22 @@ function Load_All_Discovery_Content() {
     }
 
     let HTML = `<div class="col-12 col-sm-5 col-md-4 card-container">
+            <a target="_blank" class="text-decoration-none text-reset" href="${Content.hyperlink}">
             <div class="card mb-3">
             <h3 class="card-header">${Content.header}</h3>
             <div class="card-body">
               <h5 class="card-title">${Content.title}</h5>
               <h6 class="card-subtitle text-muted">${Content.subtitle}</h6>
             </div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="d-block user-select-none"
-              width="100%"
-              height="200"
-              aria-label="Placeholder: Image cap"
-              focusable="false"
-              role="img"
-              preserveAspectRatio="xMidYMid slice"
-              viewBox="0 0 318 180"
-              style="font-size: 1.125rem; text-anchor: middle"
-            >
-              <img src="${Content.img_link}" width="100%" height="100%" fill="#868e96">
-              <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
-            </svg>
+
+              <img class="object-fit-cover" src="${Content.img_link}" width="100%" height="200px" fill="#868e96">
             <div class="card-body">
               <p class="card-text">
                 ${Content.description}
               </p>
             </div>
           </div>
+          </a>
           </div>`;
     Discovery_Container_DOM.insertAdjacentHTML("beforeend", HTML);
   });

059a1778a2e4945487575cb66aa7e91aadada1bf
Jin Liang <144455501+jinfeiliang@users.noreply.github.com>
2026-05-08 14:07:54 -0400

    Discover Information

--- Discover/discover.js
+++ Discover/discover.js
@@ -4,26 +4,30 @@ let Discovery_Content_List = [
   {
     header: "Map",
     title: "Food Bank For NYC",
-    subtitle: "Subtitle",
-    img_link: "gg",
+    subtitle: "Food Pastries Locations",
+    img_link: "../Images/Food_Bank_For_NYC_SS.png",
     hyperlink: "https://www.foodbanknyc.org/news-and-stories/",
-    description: "Esternocleidomastodeo",
+    description:
+      "introduces one of the largest anti-hunger organizations in New York City. <br> Its mission is to help ensure that all New Yorkers have stable access to food and basic resources.",
   },
   {
     header: "Data",
     title: "New York City Council",
-    subtitle: "Subtitle",
-    img_link: "gg",
+    subtitle: "Food Insecurity in NYC Map",
+    img_link:
+      "https://council.nyc.gov/wp-content/uploads/2025/05/livestream-overlay2.png",
     hyperlink: "https://council.nyc.gov/data/emergency-food-in-nyc/",
-    description: "Esternocleidomastodeo",
+    description:
+      "NYC offers emergency food assistance through hundreds of food pantries and community kitchens across the five boroughs. <br> The program helps connect residents to free meals and groceries, especially in neighborhoods facing high food insecurity.",
   },
   {
     header: "Information",
     title: "Metropolitan Transportation Authority",
-    subtitle: "Bathroom locations",
-    img_link: "gg",
+    subtitle: "Bathroom Locations",
+    img_link: "../Images/Subway_Bathroom_SS.png",
     hyperlink: "https://www.mta.info/agency/new-york-city-transit/bathrooms",
-    description: "Esternocleidomastodeo",
+    description:
+      "The NYC MTA provides public bathrooms at many subway stations across the city. <br> These facilities are regularly maintained and upgraded to improve cleanliness and accessibility.",
   },
   {
     header: "Map",
@@ -31,7 +35,8 @@ let Discovery_Content_List = [
     subtitle: "Subway MTA Maps",
     img_link: "../Images/Subway_Diagram.jpg",
     hyperlink: "https://www.mta.info/maps",
-    description: "Esternocleidomastodeo",
+    description:
+      "The NYC MTA provides subway, bus, and railroad maps to help riders navigate the transit system across all five boroughs. <br> These maps include subway routes, bus lines, accessibility information, and real-time service updates.",
   },
 ];
 
@@ -43,7 +48,7 @@ function Load_All_Discovery_Content() {
       return;
     }
 
-    let HTML = `<div class="col-12 col-sm-5 col-md-4 card-container">
+    let HTML = `<div class="col-12 col-md-5 card-container">
             <a target="_blank" class="text-decoration-none text-reset" href="${Content.hyperlink}">
             <div class="card mb-3">
             <h3 class="card-header">${Content.header}</h3>
